652bd49a698a11784f49ad70618e2bb353046e31
Marek Buchar <czbuchi@gmail.com>
2024-07-27 15:50:00 +0200

    added example of equipment tab modification

--- CarInspectorResizer.Example1/HarmonyPatches/CarInspectorPatches.cs
+++ CarInspectorResizer.Example1/HarmonyPatches/CarInspectorPatches.cs
@@ -21,44 +21,35 @@ internal static class CarInspectorPatches {
     [HarmonyPatch(typeof(CarInspector), "Awake")]
     public static void Awake(ref Window ____window) {
         var windowAutoHeight = ____window.gameObject!.GetComponent<CarInspectorAutoHeightBehavior>()!;
+        windowAutoHeight.ExpandTab("equipment", 50);
         windowAutoHeight.ExpandOrders(AutoEngineerMode.Road, 50);
-        windowAutoHeight.ExpandOrders(AutoEngineerMode.Yard, 100);
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(CarInspector), "PopulateEquipmentPanel")]
+    public static void PopulateEquipmentPanel(UIPanelBuilder builder) {
+        builder.AddSection("Equipment Buttons", section => {
+            section.ButtonStrip(strip => {
+                strip.AddButton("Example1", () => { });
+                strip.AddButton("Example1", () => { });
+                strip.AddButton("Example1", () => { });
+            }, 4);
+        });
     }
 
     [HarmonyPostfix]
     [HarmonyPatch(typeof(CarInspector), "BuildContextualOrders")]
     public static void BuildContextualOrders(UIPanelBuilder builder, AutoEngineerPersistence persistence, Car ____car) {
-
         var helper = new AutoEngineerOrdersHelper(____car, persistence);
         var mode = helper.Mode();
-        switch (mode) {
-            case AutoEngineerMode.Road:
-                builder.AddSection("Road Buttons", section => {
-                    section.ButtonStrip(strip => {
-                        strip.AddButton("Example1", () => { });
-                        strip.AddButton("Example1", () => { });
-                        strip.AddButton("Example1", () => { });
-                    }, 4);
-                });
-                break;
-
-            case AutoEngineerMode.Yard:
-                builder.AddSection("Yard Buttons", section => {
-                    section.ButtonStrip(strip => {
-                        strip.AddButton("Example1", () => { });
-                        strip.AddButton("Example1", () => { });
-                        strip.AddButton("Example1", () => { });
-                    }, 4);
-                });
-
-                builder.AddSection("More Yard buttons", section => {
-                    section.ButtonStrip(strip => {
-                        strip.AddButton("Example1", () => { });
-                        strip.AddButton("Example1", () => { });
-                        strip.AddButton("Example1", () => { });
-                    }, 4);
-                });
-                break;
+        if (mode == AutoEngineerMode.Road) {
+            builder.AddSection("Road Buttons", section => {
+                section.ButtonStrip(strip => {
+                    strip.AddButton("Example1", () => { });
+                    strip.AddButton("Example1", () => { });
+                    strip.AddButton("Example1", () => { });
+                }, 4);
+            });
         }
     }
 
